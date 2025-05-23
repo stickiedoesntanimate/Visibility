408b089f5ba3ea3cb6d77ac2a1b0457dc4da990c
InvisibleSides <invisiblesides0@gmail.com>
2025-05-23 18:34:39 +0200

    Updated Vouchers

--- src/vouchers.lua
+++ src/vouchers.lua
@@ -4,52 +4,63 @@ SMODS.Voucher {
     loc_txt = {
         name = "Warhead",
         text = {
-            "{C:attention}+#1#{} Ante,",
-            "{C:blue}+#1#{} hand",
+            "{C:attention}-#1#{} Ante,",
+            "{C:attention}-#1#{} hand size",
             "each round",
         }
     },
     pos = { x = 0, y = 0 },
-    config = { extra = { deduction = 2 } },
+    config = { extra = { deduction = 1, size = 1 } },
+    unlocked = true,
+    atlas = "TextureAtlasVouchers",
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.deduction } }
     end,
+    locked_loc_vars = function(self, info_queue, card)
+        return { vars = { 12 } }
+    end,
     redeem = function(self, card)
         -- Apply ante change
-        ease_ante(card.ability.extra.deduction)
+        ease_ante(-card.ability.extra.deduction)
         G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
-        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante + card.ability.extra.deduction
-
-        -- Apply hand change
-        G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.deduction
-        ease_hands_played(card.ability.extra.deduction)
+        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante - card.ability.extra.deduction
+        G.hand:change_size(-card.ability.extra.size)
+    end,
+    check_for_unlock = function(self, args)
+        return args.type == 'ante_up' and args.ante >= 12
     end
 }
 
 SMODS.Voucher {
     key = 'fallout',
-    atlas = "TextureAtlasVouchers",
     loc_txt = {
         name = "Fallout",
         text = {
-            "{C:attention}+#1#{} Ante,",
-            "{C:blue}+#1#{} hand",
+            "{C:attention}-#1#{} Ante,",
+            "{C:attention}-#1#{} consumable size",
             "each round",
         }
     },
     pos = { x = 1, y = 0 },
-    config = { extra = { deduction = 2 } },
+    config = { extra = { deduction = 1, slots = 1 } },
+    unlocked = true,
+    atlas = "TextureAtlasVouchers",
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.deduction } }
     end,
+    locked_loc_vars = function(self, info_queue, card)
+        return { vars = { 12 } }
+    end,
     redeem = function(self, card)
         -- Apply ante change
-        ease_ante(card.ability.extra.deduction)
+        ease_ante(-card.ability.extra.deduction)
         G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
-        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante + card.ability.extra.deduction
-
-        -- Apply hand change
-        G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.deduction
-        ease_hands_played(card.ability.extra.deduction)
+        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante - card.ability.extra.deduction
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.consumeables.config.card_limit = G.consumeables.config.card_limit - card.ability.extra.slots
+                return true
+            end
+        }))
     end
 }

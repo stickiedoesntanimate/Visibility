7afc54d26d83a5c196bfc3dd73d6a77e8ee77a41
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2025-05-24 13:11:35 +0200

    God Complex (Texture TBD)

--- src/jokers.lua
+++ src/jokers.lua
@@ -449,6 +449,7 @@ SMODS.Joker {
 		end
 	end,}
 --]]
+
 -- Lean
 SMODS.Joker {
 	key = 'lean',
@@ -499,6 +500,7 @@ SMODS.Joker {
 			"{C:inactive}(must have room)"
 		}
 	},
+	discovered = true,
 	unlocked = true,
 	blueprint_compat = true,
 	eternal_compat = true,
@@ -507,7 +509,7 @@ SMODS.Joker {
 	pools = { ["Visibility"] = true },
 	atlas = 'TextureAtlasJokers',
 	pos = { x = 0, y = 2 },
-	cost = 8,
+	cost = 3,
 	loc_vars = function(self, info_queue, card)
         return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds } }
 	end,
@@ -526,4 +528,55 @@ SMODS.Joker {
 			end
 		end
     end,
+}
+
+-- God Complex
+SMODS.Joker {
+	key = 'god_complex',
+	loc_txt = {
+		name = 'God Complex',
+		text = {
+			"At the end of round, gain {C:gold}+$#1#{}",
+			"for all {C:blue}hands{} remaining",
+			"{s:2}BUT",
+			"{C:attention}-#2# Hand Size{} every time a {C:mult}discard{} is used"
+		}
+	},
+	discovered = true,
+	unlocked = true,
+	blueprint_compat = true,
+	eternal_compat = true,
+	config = { extra = { money = 1, deduction = 1 } },
+	rarity = 3,
+	pools = { ["Visibility"] = true },
+	atlas = 'TextureAtlasJokers',
+	pos = { x = 4, y = 4 }, -- TODO: Texture
+	cost = 10,
+	loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.money, card.ability.extra.deduction } }
+	end,
+	calculate = function(self, card, context)
+		if context.pre_discard then
+			G.hand:change_size(-card.ability.extra.deduction)
+			card.ability.extra.deducted = card.ability.extra.deducted + card.ability.extra.deduction
+		end
+		if context.end_of_round and context.cardarea == G.jokers then
+			G.hand:change_size(card.ability.extra.deducted)
+			card.ability.extra.deducted = 0
+			-- Get hands remaining
+			local hands_remaining = G.GAME.current_round.hands_left
+			if hands_remaining > 0 then
+				local money = hands_remaining * card.ability.extra.money
+				return {
+					dollars = money,
+				}
+			end
+		end
+    end,
+	add_to_deck = function (self, card, from_debuff)
+		card.ability.extra.deducted = 0
+	end,
+	remove_from_deck = function (self, card, from_debuff)
+		G.hand:change_size(card.ability.extra.deducted)
+	end
 }

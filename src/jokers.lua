72eb80c558d58d02b5a8667674128f3fba23dc16
InvisibleSides <invisiblesides0@gmail.com>
2025-05-25 14:05:25 +0200

    Echo Chamber

--- src/jokers.lua
+++ src/jokers.lua
@@ -418,6 +418,40 @@ SMODS.Joker {
     end,
 }
 
+SMODS.Joker {
+	key = "echo_chamber",
+	loc_txt = {
+		name = "Echo Chamber",
+		text = {
+			"When a {C:mult}Boss Blind{} is defeated",
+			"create a {C:attention,T:tag_double}#1#"
+		}
+	},
+	rarity = 3,
+	atlas = 'TextureAtlasJokers',
+	pos = { x = 0, y = 2 },
+	cost = 3,
+	discovered = true,
+	unlocked = true,
+	blueprint_compat = true,
+	eternal_compat = true,
+	loc_vars = function(self, info_queue, back)
+		return { vars = { localize { type = 'name_text', key = 'tag_double', set = 'Tag' } } }
+	end,
+	calculate = function(self, back, context)
+		if context.end_of_round and context.game_over == false and context.main_eval and G.GAME.blind.boss then
+			G.E_MANAGER:add_event(Event({
+				func = function()
+					add_tag(Tag('tag_double'))
+					play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
+					play_sound('holo1', 1.2 + math.random() * 0.1, 0.4)
+					return true
+				end
+			}))
+		end
+	end,
+}
+
 -- Cave Man
 SMODS.Joker {
 	key = 'caveman',

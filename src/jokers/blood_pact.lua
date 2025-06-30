f01c98baec465e4f2bd57ce8dfd5f5f3d395fe64
GunnableScum <148823476+GunnableScum@users.noreply.github.com>
2025-06-30 22:01:52 +0200

    Blood Pact Bugfix

--- src/jokers/blood_pact.lua
+++ src/jokers/blood_pact.lua
@@ -16,30 +16,28 @@ SMODS.Joker {
 	end,
 	calculate = function(self, card, context)
 	    if context.destroy_card and context.cardarea == G.play then
-	        for k, v in pairs(context.scoring_hand) do
-                if v:get_id() == 6 then
-                    G.E_MANAGER:add_event(Event({
-                        trigger = 'after',
-                        delay = 0.2,
-                        func = function()
-							local _eligable_cards = {}
-							for k, v in pairs(G.hand.cards) do
-								if not v:get_seal(true) then
-									_eligable_cards[#_eligable_cards+1] = v
-								end
+			if context.destroy_card:get_id() == 6 then
+				G.E_MANAGER:add_event(Event({
+					trigger = 'after',
+					delay = 0.2,
+					func = function()
+						local _eligable_cards = {}
+						for k, v in pairs(G.hand.cards) do
+							if not v:get_seal(true) then
+								_eligable_cards[#_eligable_cards+1] = v
 							end
-							if #_eligable_cards == 0 then return true end
-                            local changed_card, card_key = pseudorandom_element(_eligable_cards, pseudoseed('blood_pact'))
-							changed_card:set_seal('Red', true)
-							changed_card:juice_up(0.3, 0.4)
-							table.remove(_eligable_cards, card_key)
-							play_sound('gold_seal')
-                            return true
-                        end
-                    }))
-					return { remove = true, message = 'It\'s a deal.' }
-				end
-            end
+						end
+						if #_eligable_cards == 0 then return true end
+						local changed_card, card_key = pseudorandom_element(_eligable_cards, pseudoseed('blood_pact'))
+						changed_card:set_seal('Red', true)
+						changed_card:juice_up(0.3, 0.4)
+						table.remove(_eligable_cards, card_key)
+						play_sound('gold_seal')
+						return true
+					end
+				}))
+				return { remove = true, message = 'It\'s a deal.' }
+			end
 	    end
 	end,
 }

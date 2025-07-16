a37d48da0e826e663d380479520c9ebb1d4041ba
InvisibleSides <invisiblesides0@gmail.com>
2025-07-16 19:47:03 +0200

    Handheld Joker Display

--- src/jokers/handheld.lua
+++ src/jokers/handheld.lua
@@ -24,5 +24,37 @@ SMODS.Joker {
                 xchips = card.ability.extra.xchips,
             }
         end
-    end
+    end,
+    joker_display_def = function (JokerDisplay)
+        --- @type JDJokerDefinition
+        return {
+            text = {
+                {
+                    border_nodes = {
+                        { text = "X" },
+                        { ref_table = "card.joker_display_values", ref_value = "x_chips", retrigger_type = "exp" }
+                    },
+                    border_colour = G.C.CHIPS
+                }
+            },
+            reminder_text = {
+                { text = "(" },
+                { ref_table = "card.joker_display_values", ref_value = "localized_text_ace"},
+                { text = ")" },
+            },
+            calc_function = function(card)
+                local count = 0
+                local text, _, scoring_hand = JokerDisplay.evaluate_hand()
+                if text ~= 'Unknown' then
+                    for _, scoring_card in pairs(scoring_hand) do
+                        if scoring_card:get_id() and scoring_card:get_id() == 14 then
+                            count = count + JokerDisplay.calculate_card_triggers(scoring_card, scoring_hand)
+                        end
+                    end
+                end
+                card.joker_display_values.x_chips = card.ability.extra.xchips * count
+                card.joker_display_values.localized_text_ace = localize("Ace", "ranks")
+            end
+        }
+    end,
 }
